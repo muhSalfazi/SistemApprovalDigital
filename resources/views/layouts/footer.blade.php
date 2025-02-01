0f72502b47e85de2d27ec5aa88d097d354855830
muhamad salman fauzi <salmanfauzi0512@gmail.com>
2025-02-01 15:03:02 +0700

    fix:footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer">
     <div class="copyright" style="text-align: center">
-        Copyright_HRGA System<strong><span> &copy;2024</span></strong>
+        Copyright_HRGA System<strong><span> &copy;2025</span></strong>
     </div>
 </footer><!-- End Footer -->
